6cdb231ca100a11b88178057377610d9a4a5f37c
Poe Rhiza <poerhiz@gmail.com>
2016-03-22 20:49:18 -0500

    test(dist): testing dist of addon, fixed build.sh warnings

--- firefox/build.sh
+++ firefox/build.sh
@@ -3,16 +3,15 @@
 # https://raw.githubusercontent.com/chutten/statuser/gh-pages/build.sh
 # RUN THIS IN THE PROJECT ROOT FOLDER TO BUILD THE XPI
 
+cd addon
 jpm xpi
 NAME=$(echo @acceptum-*.xpi)
-mv "$NAME" "dist/${NAME//@}"
+mv "$NAME" "../dist/${NAME//@}"
 
 # set the version from `package.json`'s `"version"` field
 VERSION=$(grep -o '"version"\s*:\s*"[^"]*' package.json | cut -d '"' -f 4 ) # read version from package.json
 echo "read version $VERSION from package.json"
 IFS=$'\n' # temporarily set the fields delimiter to newline
-for f in $(find -name '*.rdf.template' -or -name '*.html.template')
-do
-  echo "processing $f"
-  sed "s/{{\s*VERSION\s*}}/$VERSION/g" "$f" | sed "2s;^;<!-- DO NOT EDIT - AUTOMATICALLY GENERATED FROM: $f -->\n\n;" |  tee ${f%.*} > /dev/null
-done
+sed "s/{{\s*VERSION\s*}}/$VERSION/g" "../dist/update.rdf.template" | sed "2s;^;<!-- DO NOT EDIT - AUTOMATICALLY GENERATED FROM: ../build.sh -->\n\n;" |  tee ../dist/update.rdf > /dev/null
+stat ../dist/acceptum-${VERSION}.xpi && jpm -v sign --api-key ${JPM_API_KEY} --api-secret ${JPM_API_SECRET} --xpi ../dist/acceptum-${VERSION}.xpi
+mv acceptum-${VERSION}-fx.xpi ../dist/acceptum-${VERSION}.xpi
